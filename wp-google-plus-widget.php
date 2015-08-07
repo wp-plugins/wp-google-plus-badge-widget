5d74799121865ebb0ed6542d6539bbaccb166371
chandrakeshkumar <chandrakeshkumar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 03:16:49 +0000

    Our Google plus badge widget plugin will help you to display Google plus badge widget on your wordpress wesite, just add google plus badge widget to your sidebar and use it.
    git-svn-id: https://plugins.svn.wordpress.org/wp-google-plus-badge-widget/trunk@1214541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-google-plus-widget.php
+++ wp-google-plus-widget.php
@@ -3,7 +3,7 @@
  Plugin Name: Google+ Badge Widget 
  Plugin URI: http://www.wpchandra.com
  Description: Our Google plus badge widget plugin will help you to display Google plus badge widget on your wordpress wesite, just add google plus badge widget to your sidebar and use it. 
- Version: 1.0
+ Version: 1.0 
  Author: Chandrakesh Kumar  
  Author URI: http://www.wpchandra.com/ 
  License: GPL3 
